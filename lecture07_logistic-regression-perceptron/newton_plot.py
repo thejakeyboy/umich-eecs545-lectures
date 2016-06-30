dbb5af8e3ece0154be1431c2e3b70fb13f0306b0
dzdhr <zhedu.hust@gmail.com>
2016-06-30 01:18:38 -0400

    Revision to lec07 and Slight Changes to Previous Lectures

--- lecture07_logistic-regression-perceptron/newton_plot.py
+++ lecture07_logistic-regression-perceptron/newton_plot.py
@@ -34,7 +34,16 @@ def plot_optimization(ax, fn, d1, lst, xlim=None, ylim=None, title="", tangents=
         xx, yy = [x0, x1], [fn(x0), fn(x1)];
         #ax.plot(xx, yy, "or");
         ax.plot(xx, yy, "-r");
-    
+
+    # Plot Markers
+    for i in range(0,lst.__len__()):
+        x = lst[i]
+        xx = [x, x];
+        yy = [fn(x), 0];
+        ax.plot(xx, yy, "--c");
+        ax.plot(xx, yy, "oc");
+        ax.text(x, 0, "x_"+str(i+1), fontsize=12, horizontalalignment='center', verticalalignment='top')
+
     if xlim is not None: ax.set_xlim(xlim);
     if ylim is not None: ax.set_ylim(ylim);
     
@@ -53,3 +62,16 @@ def newton_exact(d1, d2, x0, eps=1e-10, maxn=1e2, lst=None):
         x0 = x1;
     print("Newton's Method did not converge.");
     return x0;
+
+def newton_exact_ver2(fn, d1, x0, eps=1e-10, maxn=1e2, lst=None):
+    """
+    Runs Newton's method using exact analytic derivatives.
+    @param (fn: R --> R) Analytic f(x).
+    @param (d1: R --> R) Analytic first derivative
+    """
+    for n in range(int(maxn)):
+        if lst is not None: lst.append(x0);
+        x1 = x0-fn(x0)/d1(x0);
+        if abs(x1-x0) < eps: return x0;
+        x0 = x1;
+    return lst;
